57caf9ee1fcff3542fb8ccdd605f31ead7cbf9b9
Johny709 <janjoseph.merida@gmail.com>
2026-08-06 14:37:43 -0400

    add Creative Fluid Cover GUI

--- src/main/java/tja/items/covers/CoverCreativeFluid.java
+++ src/main/java/tja/items/covers/CoverCreativeFluid.java
@@ -1,18 +1,34 @@
 package tja.items.covers;
 
+import codechicken.lib.raytracer.CuboidRayTraceResult;
 import codechicken.lib.render.CCRenderState;
 import codechicken.lib.render.pipeline.IVertexOperation;
 import codechicken.lib.vec.Cuboid6;
 import codechicken.lib.vec.Matrix4;
+import com.cleanroommc.modularui.drawable.GuiTextures;
+import com.cleanroommc.modularui.drawable.Icon;
+import com.cleanroommc.modularui.factory.SidedPosGuiData;
+import com.cleanroommc.modularui.screen.ModularPanel;
+import com.cleanroommc.modularui.screen.UISettings;
+import com.cleanroommc.modularui.value.sync.BooleanSyncValue;
+import com.cleanroommc.modularui.value.sync.InteractionSyncHandler;
+import com.cleanroommc.modularui.value.sync.PanelSyncManager;
+import com.cleanroommc.modularui.widgets.ButtonWidget;
+import com.cleanroommc.modularui.widgets.RichTextWidget;
+import com.cleanroommc.modularui.widgets.ToggleButton;
+import com.cleanroommc.modularui.widgets.slot.FluidSlot;
 import gregtech.api.capability.impl.FluidTankList;
 import gregtech.api.cover.CoverBase;
 import gregtech.api.cover.CoverDefinition;
 import gregtech.api.cover.CoverWithUI;
 import gregtech.api.cover.CoverableView;
+import gregtech.api.mui.GTGuiTextures;
+import gregtech.api.util.KeyUtil;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ITickable;
+import net.minecraft.util.*;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
@@ -40,6 +56,60 @@ public class CoverCreativeFluid extends CoverBase implements ITickable, CoverWit
     @Override
     public boolean canAttach(@Nonnull CoverableView coverableView, @Nonnull EnumFacing enumFacing) {
         return coverableView.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, enumFacing) != null;
+    }
+
+    @Nonnull
+    @Override
+    public EnumActionResult onScrewdriverClick(@Nonnull EntityPlayer player, @Nonnull EnumHand hand, @Nonnull CuboidRayTraceResult hitResult) {
+        if (!this.getWorld().isRemote)
+            this.openUI((EntityPlayerMP) player);
+        return EnumActionResult.SUCCESS;
+    }
+
+    @Override
+    public ModularPanel buildUI(SidedPosGuiData guiData, PanelSyncManager guiSyncManager, UISettings settings) {
+        guiSyncManager.syncValue("is_working", new BooleanSyncValue(() -> this.isWorking, this::setWorking));
+        guiSyncManager.syncValue("speed_multiply", new InteractionSyncHandler()
+                .setOnMousePressed(mouseData -> this.setSpeed(this.speed * 2L)));
+        guiSyncManager.syncValue("speed_divide", new InteractionSyncHandler()
+                .setOnMousePressed(mouseData -> this.setSpeed(this.speed / 2)));
+
+        final Icon detail = GTGuiTextures.BUTTON_POWER_DETAIL.asIcon().size(18, 6).marginTop(24);
+        final ModularPanel panel = ModularPanel.defaultPanel("creative_item_cover_gui", 176, 187);
+        for (int i = 0; i < this.fluidFilter.getTanks(); i++) {
+            panel.child(new FluidSlot()
+                    .left(61 + (18 * (i % 3)))
+                    .top(25 + (18 * (i / 3)))
+                    .syncHandler(this.fluidFilter.getTankAt(i).getDelegate()));
+        }
+        return panel.bindPlayerInventory()
+                .child(new RichTextWidget()
+                        .size(54, 18)
+                        .pos(63, 80)
+                        .autoUpdate(true)
+                        .textBuilder(richText -> richText.addLine(KeyUtil.lang(TextFormatting.WHITE, "tja.machine.universal.ticks", this.speed)))
+                        .background(GTGuiTextures.DISPLAY))
+                .child(new ButtonWidget<>()
+                        .size(18)
+                        .pos(43, 80)
+                        .overlay(KeyUtil.string("-"))
+                        .background(GuiTextures.MC_BUTTON)
+                        .hoverBackground(GuiTextures.MC_BUTTON_HOVERED)
+                        .syncHandler("speed_divide"))
+                .child(new ButtonWidget<>()
+                        .size(18)
+                        .pos(116, 80)
+                        .overlay(KeyUtil.string("+"))
+                        .background(GuiTextures.MC_BUTTON)
+                        .hoverBackground(GuiTextures.MC_BUTTON_HOVERED)
+                        .syncHandler("speed_multiply"))
+                .child(new ToggleButton()
+                        .size(18)
+                        .pos(152, 80)
+                        .disableHoverBackground()
+                        .overlay(true, detail, GTGuiTextures.BUTTON_POWER[1])
+                        .overlay(false, detail, GTGuiTextures.BUTTON_POWER[0])
+                        .syncHandler("is_working"));
     }
 
     @Override
@@ -74,5 +144,15 @@ public class CoverCreativeFluid extends CoverBase implements ITickable, CoverWit
         this.isWorking = data.getBoolean("isWorking");
         if (data.hasKey("speed"))
             this.speed = data.getInteger("speed");
+    }
+
+    public void setWorking(boolean isWorking) {
+        this.isWorking = isWorking;
+        this.markAsDirty();
+    }
+
+    public void setSpeed(long speed) {
+        this.speed = (int) Math.max(1, Math.min(Integer.MAX_VALUE, speed));
+        this.markAsDirty();
     }
 }
