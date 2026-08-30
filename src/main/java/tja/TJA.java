d24e00053d28c4619056b4cdf8b1b944b7d21a1b
Johny709 <janjoseph.merida@gmail.com>
2026-08-30 08:46:41 -0400

    v0.1

--- src/main/java/tja/TJA.java
+++ src/main/java/tja/TJA.java
@@ -21,7 +21,7 @@ import tja.textures.TJATextures;
 public class TJA {
     public static final String MOD_ID = "tja";
     public static final String MOD_NAME = "TJ Additions CEU";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.1";
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
     /**
