b94cd17268b55e94ebc1d65a9bb19dbeef9162e1
Johny709 <janjoseph.merida@gmail.com>
2026-09-10 08:18:01 -0400

    simplify HWYLA info providers

--- src/main/java/tja/integration/hwyla/providers/HeatInfoDataProvider.java
+++ src/main/java/tja/integration/hwyla/providers/HeatInfoDataProvider.java
@@ -1,7 +1,5 @@
 package tja.integration.hwyla.providers;
 
-import gregtech.api.metatileentity.MetaTileEntity;
-import gregtech.api.metatileentity.MetaTileEntityHolder;
 import mcp.mobius.waila.api.*;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -10,58 +8,51 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
 import tja.capability.IHeatInfo;
 import tja.capability.TJACapabilities;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class HeatInfoDataProvider implements IWailaDataProvider {
+public class HeatInfoDataProvider extends CapabilityInfoDataProvider<IHeatInfo> {
 
     public static final HeatInfoDataProvider INSTANCE = new HeatInfoDataProvider();
 
     public void register(IWailaRegistrar registrar) {
+        super.register(registrar);
         registrar.registerNBTProvider(this, TileEntity.class);
         registrar.registerBodyProvider(this, TileEntity.class);
-        registrar.addConfig("TJA", "tja.heatinfo");
+    }
+
+    @Override
+    public Capability<IHeatInfo> getCapability() {
+        return TJACapabilities.CAPABILITY_HEAT;
+    }
+
+    @Override
+    public String getConfigName() {
+        return "tja.heatinfo";
     }
 
     @Nonnull
     @Override
-    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
-        if (!(te instanceof MetaTileEntityHolder))
-            return tag;
-        final MetaTileEntity metaTileEntity = ((MetaTileEntityHolder) te).getMetaTileEntity();
-        if (metaTileEntity == null)
-            return tag;
-        final IHeatInfo heatInfo = metaTileEntity.getCapability(TJACapabilities.CAPABILITY_HEAT, null);
-        if (heatInfo == null)
-            return tag;
-        final NBTTagCompound compound = new NBTTagCompound();
-        compound.setLong("heat", heatInfo.heat());
-        compound.setLong("maxHeat", heatInfo.maxHeat());
-        tag.setTag("tja.heatinfo", compound);
-        return tag;
-    }
-
-    @Nonnull
-    @Override
-    public List<String> getWailaBody(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-        if (!config.getConfig("tja.heatinfo"))
-            return tooltip;
-        if (!(accessor.getTileEntity() instanceof MetaTileEntityHolder))
-            return tooltip;
-        final MetaTileEntity metaTileEntity = ((MetaTileEntityHolder) accessor.getTileEntity()).getMetaTileEntity();
-        if (metaTileEntity == null)
-            return tooltip;
-        final IHeatInfo heatInfo = metaTileEntity.getCapability(TJACapabilities.CAPABILITY_HEAT, null);
-        if (heatInfo == null)
-            return tooltip;
+    public List<String> getWailaBody(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config, IHeatInfo heatInfo) {
         final NBTTagCompound compound = accessor.getNBTData().getCompoundTag("tja.heatinfo");
         final long maxHeat = compound.getLong("maxHeat");
         final long heat = Math.min(maxHeat, compound.getLong("heat"));
         tooltip.add(SpecialChars.getRenderString("tja.progressinfo", I18n.format("tja.top.progress.heat"),
                 String.valueOf(heat), String.valueOf(maxHeat), "°C", "°C", "RED", ",###"));
         return tooltip;
+    }
+
+    @Nonnull
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos, IHeatInfo heatInfo) {
+        final NBTTagCompound compound = new NBTTagCompound();
+        compound.setLong("heat", heatInfo.heat());
+        compound.setLong("maxHeat", heatInfo.maxHeat());
+        tag.setTag(this.getConfigName(), compound);
+        return tag;
     }
 }
--- src/main/java/tja/integration/hwyla/providers/RecipeInfoDataProvider.java
+++ src/main/java/tja/integration/hwyla/providers/RecipeInfoDataProvider.java
@@ -1,7 +1,6 @@
 package tja.integration.hwyla.providers;
 
 import gregtech.api.GTValues;
-import gregtech.api.metatileentity.MetaTileEntity;
 import mcp.mobius.waila.api.*;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -11,91 +10,87 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.fluids.FluidStack;
 import tja.TJAValues;
 import tja.capability.IRecipeInfo;
 import tja.capability.TJACapabilities;
-import tja.machines.MetaTileEntityUtils;
 import tja.util.TJAUtility;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 
-public class RecipeInfoDataProvider implements IWailaDataProvider {
+public class RecipeInfoDataProvider extends CapabilityInfoDataProvider<IRecipeInfo> {
 
     public static final RecipeInfoDataProvider INSTANCE = new RecipeInfoDataProvider();
 
     public void register(IWailaRegistrar registrar) {
+        super.register(registrar);
         registrar.registerBodyProvider(this, TileEntity.class);
         registrar.registerNBTProvider(this, TileEntity.class);
-        registrar.addConfig("TJA", "tja.recipeinfo");
+    }
+
+    @Override
+    public Capability<IRecipeInfo> getCapability() {
+        return TJACapabilities.CAPABILITY_RECIPE_INFO;
+    }
+
+    @Override
+    public String getConfigName() {
+        return "tja.recipeinfo";
     }
 
     @Nonnull
     @Override
-    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
-        final MetaTileEntity metaTileEntity = MetaTileEntityUtils.getMetaTileEntityAt(world, pos);
-        if (metaTileEntity != null) {
-            final IRecipeInfo recipeInfo = metaTileEntity.getCapability(TJACapabilities.CAPABILITY_RECIPE_INFO, null);
-            if (recipeInfo != null) {
-                final NBTTagCompound compound = new NBTTagCompound();
-                final NBTTagList itemInputs = new NBTTagList();
-                final NBTTagList itemOutputs = new NBTTagList();
-                final NBTTagList fluidInputs = new NBTTagList();
-                final NBTTagList fluidOutputs = new NBTTagList();
-                for (ItemStack input : recipeInfo.getItemInputs())
-                    itemInputs.appendTag(input.serializeNBT());
-                for (ItemStack output : recipeInfo.getItemOutputs())
-                    itemOutputs.appendTag(output.serializeNBT());
-                for (FluidStack input : recipeInfo.getFluidInputs())
-                    fluidInputs.appendTag(input.writeToNBT(new NBTTagCompound()));
-                for (FluidStack output : recipeInfo.getFluidOutputs())
-                    fluidOutputs.appendTag(output.writeToNBT(new NBTTagCompound()));
-                compound.setTag("itemInputs", itemInputs);
-                compound.setTag("itemOutputs", itemOutputs);
-                compound.setTag("fluidInputs", fluidInputs);
-                compound.setTag("fluidOutputs", fluidOutputs);
-                compound.setLong("energyPerTick", recipeInfo.getEnergyPerTick());
-                compound.setBoolean("active", recipeInfo.isActive());
-                compound.setBoolean("problem", recipeInfo.hasProblem());
-                tag.setTag("tja.recipeinfo", compound);
-            }
-        }
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos, IRecipeInfo recipeInfo) {
+        final NBTTagCompound compound = new NBTTagCompound();
+        final NBTTagList itemInputs = new NBTTagList();
+        final NBTTagList itemOutputs = new NBTTagList();
+        final NBTTagList fluidInputs = new NBTTagList();
+        final NBTTagList fluidOutputs = new NBTTagList();
+        for (ItemStack input : recipeInfo.getItemInputs())
+            itemInputs.appendTag(input.serializeNBT());
+        for (ItemStack output : recipeInfo.getItemOutputs())
+            itemOutputs.appendTag(output.serializeNBT());
+        for (FluidStack input : recipeInfo.getFluidInputs())
+            fluidInputs.appendTag(input.writeToNBT(new NBTTagCompound()));
+        for (FluidStack output : recipeInfo.getFluidOutputs())
+            fluidOutputs.appendTag(output.writeToNBT(new NBTTagCompound()));
+        compound.setTag("itemInputs", itemInputs);
+        compound.setTag("itemOutputs", itemOutputs);
+        compound.setTag("fluidInputs", fluidInputs);
+        compound.setTag("fluidOutputs", fluidOutputs);
+        compound.setLong("energyPerTick", recipeInfo.getEnergyPerTick());
+        compound.setBoolean("active", recipeInfo.isActive());
+        compound.setBoolean("problem", recipeInfo.hasProblem());
+        tag.setTag(this.getConfigName(), compound);
         return tag;
     }
 
     @Nonnull
     @Override
-    public List<String> getWailaBody(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
-        if (config.getConfig("tja.recipeinfo")) {
-            final MetaTileEntity metaTileEntity = MetaTileEntityUtils.getMetaTileEntityAt(accessor.getWorld(), accessor.getPosition());
-            if (metaTileEntity != null) {
-                final IRecipeInfo recipeInfo = metaTileEntity.getCapability(TJACapabilities.CAPABILITY_RECIPE_INFO, accessor.getSide());
-                if (recipeInfo != null) {
-                    final NBTTagCompound compound = accessor.getNBTData().getCompoundTag("tja.recipeinfo");
-                    final long energyPerTick = compound.getLong("energyPerTick");
-                    if (energyPerTick > 0)
-                        tooltip.add(I18n.format("tja.machine.universal.eut", TJAValues.thousandFormat.format(energyPerTick),
-                                GTValues.VOCNF[TJAUtility.getTierByVoltage(energyPerTick)]));
-                    if (compound.getBoolean("problem")) {
-                        tooltip.add(I18n.format("tja.machine.universal.has_problems"));
-                    } else if (compound.getBoolean("active")) {
-                        tooltip.add(I18n.format("tja.machine.universal.running"));
-                    }
-                    final NBTTagList itemInputs = compound.getTagList("itemInputs", 10);
-                    final NBTTagList itemOutputs = compound.getTagList("itemOutputs", 10);
-                    final NBTTagList fluidInputs = compound.getTagList("fluidInputs", 10);
-                    final NBTTagList fluidOutputs = compound.getTagList("fluidOutputs", 10);
-                    if (!itemInputs.isEmpty() || !fluidInputs.isEmpty()) {
-                        tooltip.add(I18n.format("tja.top.inputs"));
-                        tooltip.add(SpecialChars.getRenderString("tja.recipeinfo", "input"));
-                    }
-                    if (!itemOutputs.isEmpty() || !fluidOutputs.isEmpty()) {
-                        tooltip.add(I18n.format("tja.top.outputs"));
-                        tooltip.add(SpecialChars.getRenderString("tja.recipeinfo", "output"));
-                    }
-                }
-            }
+    public List<String> getWailaBody(ItemStack itemStack, List<String> tooltip, IWailaDataAccessor accessor, IWailaConfigHandler config, IRecipeInfo recipeInfo) {
+        final NBTTagCompound compound = accessor.getNBTData().getCompoundTag(this.getConfigName());
+        final long energyPerTick = compound.getLong("energyPerTick");
+        if (energyPerTick > 0)
+            tooltip.add(I18n.format("tja.machine.universal.eut", TJAValues.thousandFormat.format(energyPerTick),
+                    GTValues.VOCNF[TJAUtility.getTierByVoltage(energyPerTick)]));
+        if (compound.getBoolean("problem")) {
+            tooltip.add(I18n.format("tja.machine.universal.has_problems"));
+        } else if (compound.getBoolean("active")) {
+            tooltip.add(I18n.format("tja.machine.universal.running"));
+        }
+        final NBTTagList itemInputs = compound.getTagList("itemInputs", 10);
+        final NBTTagList itemOutputs = compound.getTagList("itemOutputs", 10);
+        final NBTTagList fluidInputs = compound.getTagList("fluidInputs", 10);
+        final NBTTagList fluidOutputs = compound.getTagList("fluidOutputs", 10);
+        if (!itemInputs.isEmpty() || !fluidInputs.isEmpty()) {
+            tooltip.add(I18n.format("tja.top.inputs"));
+            tooltip.add(SpecialChars.getRenderString("tja.recipeinfo", "input"));
+        }
+        if (!itemOutputs.isEmpty() || !fluidOutputs.isEmpty()) {
+            tooltip.add(I18n.format("tja.top.outputs"));
+            tooltip.add(SpecialChars.getRenderString("tja.recipeinfo", "output"));
         }
         return tooltip;
     }
